64c2a0e46e87baa7b227c748c0211c72f84ac60c
Khailas R <khailas303@gmail.com>
2021-08-31 07:06:41 +0530

    Update Spotify_recommender.py

--- Spotify_recommender.py
+++ Spotify_recommender.py
@@ -13,12 +13,3 @@ class Recommender:
             print(saying)
 
     rec()
-
-# playlists = sp.user_playlists('spotify')
-# while playlists:
-#     for i, playlist in enumerate(playlists['items']):
-#         print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-#     if playlists['next']:
-#         playlists = sp.next(playlists)
-#     else:
-#         playlists = None
